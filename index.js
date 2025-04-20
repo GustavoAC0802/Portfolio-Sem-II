f3a89605ac1fda33fed6b92ca6b41f8d6eb9366e
Gustavo Almeida Camargo <88459386+GustavoAC0802@users.noreply.github.com>
2025-04-20 17:52:02 -0300

    Finalização do Portfolio

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    var nome = 'Gustavo Almeida Camargo';
+    var lang = 'C#';
+    var faculdade = 'FATEC';
+    var facul = 'Anhanguera'; 
+
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        faculdade:faculdade,
+        facul:facul
+        
+    });
 });
 
-app.get("/habilidades", (req, res) => {
-    res.render("habilidades");  
+app.get("/projeto", (req, res) => {
+    res.render("projeto");  
 });
 
 app.listen(3000, () => {
